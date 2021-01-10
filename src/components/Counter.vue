7cbc78b793d0427b194c82587cc1ae521d48c964
hermannmanuel <hermann.manuel>
2021-01-11 00:04:25 +0100

    Création du contenu du composant "Counter". (Ajout d'un titre et de deux buttons).

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>{{ currentValue }}</h2>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        currentValue: 0
+      }
+    },   
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
