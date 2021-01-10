638fd9cc8e749d267599cdd7f9c1d3329839d201
hermannmanuel <hermann.manuel>
2021-01-11 00:08:04 +0100

    Création de deux méthodes (Increment et Decrement)

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>{{ currentValue }}</h2>
+    <h2>Counter : {{ currentValue }}</h2>
     <button @click="increment">+</button>
     <button @click="decrement">-</button>
 
@@ -13,7 +13,16 @@
       return{
         currentValue: 0
       }
-    },   
+    }, 
+    methods: {
+      increment() {
+        this.currentValue = this.currentValue + 1;
+      },
+      decrement() {
+        this.currentValue = this.currentValue - 1;
+      }
+
+    }  
   }
 </script>
 
